bea9c29f714330f010405ab9278c7016a6e4dc0e
robinn_yk <robinsonalessandro3@hotmail.com>
2026-01-08 23:07:00 -0500

    base de datos DAC

--- Script/DDL_Exobot.sql
+++ Script/DDL_Exobot.sql
@@ -1,5 +1,7 @@
+-- database: ../Database/EXOBOT.sqlite
+    
+    PRAGMA foreign_keys = ON;
 
-    -- Database: ../Database/EXOBOT.sqlite
 
 
     DROP TABLE IF EXISTS trPersona;
@@ -15,8 +17,8 @@
 
     CREATE TABLE trCatalogoTipo (
         trIdCatalogoTipo  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        trNombre          VARCHAR(10) NOT NULL UNIQUE,
-        trDescripcion     VARCHAR(90) NOT NULL UNIQUE,
+        trNombre          VARCHAR(50) NOT NULL,
+        trDescripcion     VARCHAR(90),
         trEstado          VARCHAR(1) NOT NULL DEFAULT 'A',
         trFechaCreacion   DATETIME DEFAULT CURRENT_TIMESTAMP,
         trFechaModifica   DATETIME
@@ -25,11 +27,13 @@
     CREATE TABLE trCatalogo (
         trIdCatalogo      INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         trIdCatalogoTipo  INTEGER NOT NULL REFERENCES trCatalogoTipo(trIdCatalogoTipo),
-        trNombre          VARCHAR(10) NOT NULL UNIQUE,
-        trDescripcion     VARCHAR(90) NOT NULL UNIQUE,
+        trNombre          VARCHAR(10) NOT NULL,
+        trDescripcion     VARCHAR(90),
         trEstado          VARCHAR(1) NOT NULL DEFAULT 'A',
         trFechaCreacion   DATETIME DEFAULT (datetime('now', 'localtime')),
-        trFechaModifica   DATETIME
+        trFechaModifica   DATETIME,
+
+        UNIQUE (trIdCatalogoTipo, trNombre)
     );
 
     CREATE TABLE trIABot (
@@ -44,24 +48,35 @@
         trIdIABot         INTEGER NOT NULL,
         trNombre          TEXT NOT NULL,
         trSerie           TEXT NOT NULL,
-        trDescripcion     TEXT, 
-        CONSTRAINT        FK_trIABot
-        FOREIGN KEY       (trIdIABot) 
-        REFERENCES        trIABot(trIdIABot)
+        trDescripcion     TEXT,
+        CONSTRAINT FK_trIABot FOREIGN KEY (trIdIABot) 
+            REFERENCES trIABot(trIdIABot)
     );
 
     CREATE TABLE trPersona (
-        trIdPersona                      INTEGER PRIMARY KEY AUTOINCREMENT,
-        trIdCatalogoPersona              INTEGER NOT NULL REFERENCES trCatalogo(trIdCatalogoPersona),
-        trIdCatalogoSexo                 INTEGER NOT NULL REFERENCES trCatalogo(trIdCatalogoSexo),
-        trIdCatalogoEstadoCivil          INTEGER NOT NULL REFERENCES trCatalogo(trIdCatalogoEstadoCivil),
+        trIdPersona                      INTEGER PRIMARY KEY AUTOINCREMENT, 
+
+        trIdCatalogoPersona              INTEGER NOT NULL,
+        trIdCatalogoSexo                 INTEGER NOT NULL,
+        trIdCatalogoEstadoCivil          INTEGER NOT NULL,
+
         trNombre                         VARCHAR(50) NOT NULL,
         trApellido                       VARCHAR(50) NOT NULL,
         trCedula                         VARCHAR(10) NOT NULL UNIQUE,
 
         trEstado                         VARCHAR(1) NOT NULL DEFAULT 'A',
         trFechaCreacion                  DATETIME DEFAULT (datetime('now', 'localtime')),
-        trFechaModifica                  DATETIME
+        trFechaModifica                  DATETIME,
+
+        FOREIGN KEY (trIdCatalogoPersona) 
+            REFERENCES trCatalogo(trIdCatalogo),
+
+        FOREIGN KEY (trIdCatalogoSexo) 
+            REFERENCES trCatalogo(trIdCatalogo),
+
+        FOREIGN KEY (trIdCatalogoEstadoCivil) 
+            REFERENCES trCatalogo(trIdCatalogo)
+
     );
 
 
--- Script/DML_Exobot.sql
+++ Script/DML_Exobot.sql
@@ -1,3 +1,4 @@
+-- database: ../Database/EXOBOT.sqlite
 
 INSERT INTO trCatalogoTipo (trNombre, trDescripcion) VALUES
 ( 'Tipo Persona'    , 'Tipos: soldado, mecanico, expertos, ...'),
@@ -6,22 +7,41 @@ INSERT INTO trCatalogoTipo (trNombre, trDescripcion) VALUES
 ( 'Raza'            , 'Tipos: negro, blanco, mestizo, ...');
 
 INSERT INTO trCatalogo (trIdCatalogoTipo, trNombre, trDescripcion) VALUES
-( 1,'Soldado', 'Tipo de persona del ejercito'),
-( 1,'Mecanico', 'Tipo de persona del ejercito'),
-( 1,'Experto Ing.', 'Tipo de persona del ejercito'),
-( 1,'Experto Esp.', 'Tipo de persona del ejercito'),
+( 1,'Soldado', 'Tipo de persona del ejercito'),                -- 1
+( 1,'Mecanico', 'Tipo de persona del ejercito'),               -- 2
+( 1,'Experto Ing.', 'Tipo de persona del ejercito'),           -- 3
+( 1,'Experto Esp.', 'Tipo de persona del ejercito'),           -- 4
 
-( 2,'Masculino', 'Tipo de sexualidad'),
-( 2,'Femenino', 'Tipo de sexualidad'),
-( 2,'Hibrido', 'Tipo de sexualidad'),
-( 2,'Asexual', 'Tipo de sexualidad'),
+( 2,'Masculino', 'Tipo de sexualidad'),                        -- 5
+( 2,'Femenino', 'Tipo de sexualidad'),                         -- 6
+( 2,'Hibrido', 'Tipo de sexualidad'),                          -- 7
+( 2,'Asexual', 'Tipo de sexualidad'),                          -- 8 
 
-( 3,'Soltero', 'Tipo de estado civil Ecuador'),
-( 3,'Casado', 'Tipo de estado civil Ecuador'),
-( 3,'Viudo', 'Tipo de estado civil Ecuador'),
-( 3,'Divorciado', 'Tipo de estado civil Ecuador'),
+( 3,'Soltero', 'Tipo de estado civil Ecuador'),                -- 9
+( 3,'Casado', 'Tipo de estado civil Ecuador'),                 -- 10
+( 3,'Viudo', 'Tipo de estado civil Ecuador'),                  -- 11
+( 3,'Divorciado', 'Tipo de estado civil Ecuador'),             -- 12
 
-( 4,'Negro', 'Tipo de raza'),
+( 4,'Negro', 'Tipo de raza'),                                  
 ( 4,'Blanco', 'Tipo de raza'),
 ( 4,'Mestizo', 'Tipo de raza'),
-( 4,'Otro', 'Tipo de raza'),
+( 4,'Otro', 'Tipo de raza');   
+
+INSERT INTO trIABot (trNombre,trDescripcion) VALUES
+( "IA_RUSO", "Inteligencia Artificial");
+
+INSERT INTO trExobot (trIdIAbot, trNombre, trSerie) VALUES
+( 1, "Exobot_001", "Serie E1"),
+( 1, "Exobot_002", "Serie E2"),
+( 1, "Exobot_003", "Serie E3");
+
+INSERT INTO trPersona 
+(trIdCatalogoPersona, trIdCatalogoSexo, trIdCatalogoEstadoCivil, trNombre, trApellido, trCedula) VALUES
+(1,                   5,                    9,                    'Juan',   'Pérez',   '1234567890'),
+(2,                   5,                    10,                   'María',  'Gómez',   '0987654321'),
+(3,                   5,                    11,                   'Carlos', 'Ramírez', '1122334455'),
+(4,                   5,                    12,                   'Ana',    'López',   '5566778899'),
+(1,                   6,                    9,                    'Pepito',   'Putin',   '9998887776'),
+(2,                   6,                    10,                   'Giuliana',  'Torres',   '1654321098'),
+(3,                   6,                    11,                   'Eva', 'Reyes', '1567434455'),
+(4,                   6,                    12,                   'Perla',    'Villa',   '1366778899');
